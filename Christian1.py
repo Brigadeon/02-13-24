5bbfc5c65dcedadca87c61725c005533189ed618
Brigadeon <gadianochristian@gmail.com>
2024-02-13 14:38:50 +0800

    Here you go

---
+++ Christian1.py
@@ -0,0 +1,18 @@
+
+
+
+while True:
+
+	user_input = input("Enter Something: ").lower()
+
+	if user_input == "hi":
+		print("Hello!")
+
+	elif user_input == 'bad':
+		print('Why!?!?')
+
+	elif user_input == 'good'
+		print('Good to hear')
+
+	elif user_input == 'nevermind':
+		print('Oh ok.')
